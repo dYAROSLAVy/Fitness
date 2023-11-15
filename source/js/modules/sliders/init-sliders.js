586646667ec8daa98c825d0585f4728bdb68bb35
dYAROSLAVy <yadsp@yandex.ru>
2023-11-15 23:21:49 +0300

    pages

--- source/js/modules/sliders/init-sliders.js
+++
@@ -1,9 +0,0 @@
-import {jurySlider} from './jury-slider';
-import {reviewsSlider} from './reviews-slider';
-
-const initSliders = () => {
-  jurySlider();
-  reviewsSlider();
-};
-
-export {initSliders};
